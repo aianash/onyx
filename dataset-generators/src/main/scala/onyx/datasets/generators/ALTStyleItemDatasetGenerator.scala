2ec4fc2a76b0c6760227c9b5a1294678bf5205e0
Neeraj Gangwar <y.neeraj2008@gmail.com>
2015-10-31 09:53:20 +0530

    add: item pic in RECL for alt style item dataset generator

--- dataset-generators/src/main/scala/onyx/datasets/generators/ALTStyleItemDatasetGenerator.scala
+++ dataset-generators/src/main/scala/onyx/datasets/generators/ALTStyleItemDatasetGenerator.scala
@@ -4,6 +4,11 @@ package generators
 
 import scala.io.Source
 
+import java.net.URL
+import javax.swing.{JDialog, JLabel, ImageIcon}
+import java.awt.Dimension
+import java.awt.Toolkit
+
 import org.rogach.scallop._
 
 import play.api.libs.json._
@@ -31,7 +36,18 @@ object ALTStyleItemDatasetGenerator {
     val intentDataset      = IntentDataset(conf.intentdataset())
     val itemFeatureDataset = ALTItemRelevanceDataset(conf.output())
 
-    ALTItemRelevanceRECL(intentDataset, itemjson).start(itemFeatureDataset)
+    // set dialog to display the product image
+    val dialog = new JDialog
+    val label  = new JLabel
+
+    dialog.setUndecorated(false)
+    dialog.setAlwaysOnTop(true)
+    dialog.setFocusable(false)
+    dialog.setFocusableWindowState(false)
+    dialog.add(label)
+    dialog.setVisible(true)
+
+    ALTItemRelevanceRECL(intentDataset, itemjson).start(itemFeatureDataset, dialog, label)
   }
 
   /**
@@ -40,15 +56,15 @@ object ALTStyleItemDatasetGenerator {
    * @param {String} path to clothing item json file
    */
   case class ALTItemRelevanceRECL(intentDataset: IntentDataset, path: String)
-    extends RECL[ALTItemRelevanceDataset, (ALT, String), Char, (ALT, ItemFeature)] {
+    extends RECL[(ALTItemRelevanceDataset, JDialog, JLabel), (ALT, String), Char, (ALT, ItemFeature)] {
 
-    val source  = new RECL.Source[(ALT, String), ALTItemRelevanceDataset] {
-      def apply(dataset: ALTItemRelevanceDataset) =
+    val source  = new RECL.Source[(ALT, String), (ALTItemRelevanceDataset, JDialog, JLabel)] {
+      def apply(context: (ALTItemRelevanceDataset, JDialog, JLabel)) =
         intentDataset.generateRandomALT.asInstanceOf[RandomIterator[ALT]] <+> Source.fromFile(path).getLines.toList
     }
 
-    val eval = new RECL.Eval[ALTItemRelevanceDataset, (ALT, String), (ALT, ItemFeature)] {
-      def apply(dataset: ALTItemRelevanceDataset) = (altItemT : (ALT, String)) => {
+    val eval = new RECL.Eval[(ALTItemRelevanceDataset, JDialog, JLabel), (ALT, String), (ALT, ItemFeature)] {
+      def apply(context: (ALTItemRelevanceDataset, JDialog, JLabel)) = (altItemT : (ALT, String)) => {
         val itemJson = Json.parse(altItemT._2)
         val itg      = ItemTypeGroup((itemJson \ "itemTypeGroup").as[String])
         val styles   = ClothingStyles((itemJson \ "styles").as[Seq[String]] map { x => ClothingStyle(x) })
@@ -58,15 +74,33 @@ object ALTStyleItemDatasetGenerator {
         val tips     = StylingTips((itemJson \ "stylingTips").as[String])
         val descr    = Description((itemJson \ "descr").as[String])
         val feature  = ItemFeature(itg, styles, fabric, fit, color, tips, descr)
-        Some(s"How relevant is ALT ${altItemT._1} for item ${altItemT._2} ?" -> (altItemT._1, feature))
+        val imageUrl = new URL((itemJson \ "images" \ "primary").as[String])
+
+        // show image in dialog
+        val img = new ImageIcon(imageUrl)
+        context._3.setIcon(img)
+        context._2.pack
+
+        // set window position
+        val toolkil    = Toolkit.getDefaultToolkit
+        val screenSize = toolkil.getScreenSize
+        context._2.setLocation(screenSize.width - context._2.getWidth, screenSize.height - context._2.getHeight)
+
+        // confirmation message
+        var msg = s"How relevant is the dress shown in the image for ${altItemT._1.activity.value.toUpperCase} to look "
+        msg += s"${altItemT._1.look.value.toUpperCase} on/in ${altItemT._1.timeWeather.value.toUpperCase}?\n"
+        msg += s"Item Features:\n"
+        msg += s"${feature}\n"
+
+        Some(s"\033[H\033[2J\n\n${msg}" -> (altItemT._1, feature))
       }
     }
 
     val confirm = RECL.one23Confirm
 
-    val sink = new RECL.Sink[ALTItemRelevanceDataset, (ALT, String), Char, (ALT, ItemFeature)] {
-      def apply(dataset: ALTItemRelevanceDataset) = {
-        case Left((_, altFeatureT, choice)) => dataset += ((altFeatureT._1, altFeatureT._2, relevance(choice)))
+    val sink = new RECL.Sink[(ALTItemRelevanceDataset, JDialog, JLabel), (ALT, String), Char, (ALT, ItemFeature)] {
+      def apply(context: (ALTItemRelevanceDataset, JDialog, JLabel)) = {
+        case Left((_, altFeatureT, choice)) => context._1 += ((altFeatureT._1, altFeatureT._2, relevance(choice)))
         case _ =>
       }
     }
